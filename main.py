2e076eb8203faeecb3093ae3502e04df37747450
Baptiste Angot <bat76640@gmail.com>
2023-05-26 20:47:58 +0200

    Create API route to send file

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
+from flask import Flask, request, jsonify
 import tensorflow as tf
 import tensorflow_hub as hub
 import numpy as np
 from PIL import Image
 import csv
 
+app = Flask(__name__)
+
 # Charger le modèle
 model = hub.load("https://tfhub.dev/google/aiy/vision/classifier/plants_V1/1")
 
 # Charger et prétraiter l'image
-def preprocess_image(image_path):
-    image = Image.open(image_path).resize((224, 224))  # Redimensionner l'image
+def preprocess_image(image):
+    image = image.resize((224, 224))  # Redimensionner l'image
     image = np.array(image) / 255.0  # Normaliser les valeurs de pixel entre 0 et 1
     return image
 
 # Prédiction de l'image
-def predict_image(image_path):
-    preprocessed_image = preprocess_image(image_path)
+def predict_image(image):
+    preprocessed_image = preprocess_image(image)
     expanded_dims_image = np.expand_dims(preprocessed_image, axis=0)  # Ajouter une dimension supplémentaire pour l'entrée du modèle
     predictions = model.signatures["default"](tf.constant(expanded_dims_image, dtype=tf.float32))
     predicted_class = np.argmax(predictions["default"], axis=-1)[0]  # Trouver la classe prédite avec la plus haute probabilité
@@ -32,11 +35,20 @@ def get_class_name(class_id):
                 return row[1]
     return "Classe non trouvée"
 
-# Chemin vers l'image à prédire
-image_path = "romulearosea.jpg"
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image file provided'})
 
-# Prédire l'image
-predicted_class, probability = predict_image(image_path)
-class_name = get_class_name(predicted_class)
-print("Classe prédite :", class_name)
-print("Pourcentage de précision :", probability * 100)
+    image = Image.open(request.files['image'])
+    predicted_class, probability = predict_image(image)
+    class_name = get_class_name(predicted_class)
+
+    response = {
+        'class_name': class_name,
+        'probability': float(probability)
+    }
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run()
